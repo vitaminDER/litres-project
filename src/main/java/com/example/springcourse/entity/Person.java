29e306b05ea421f7dea251ecb77cbf5676bd01ce
Efim Goncharov <efim.goncharov@bk.ru>
2025-01-09 18:26:52 +0500

    person class done

---
+++ src/main/java/com/example/springcourse/entity/Person.java
@@ -0,0 +1,37 @@
+package com.example.springcourse.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Person")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "email")
+    private String email;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Book> books;
+
+}
