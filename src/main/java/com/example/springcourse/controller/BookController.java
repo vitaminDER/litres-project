899af9dc582566d27b9853f6968942da9162f498
Efim Goncharov <efim.goncharov@bk.ru>
2025-01-10 22:03:39 +0500

    change BookController.java

--- src/main/java/com/example/springcourse/controller/BookController.java
+++ src/main/java/com/example/springcourse/controller/BookController.java
@@ -1,34 +1,32 @@
 package com.example.springcourse.controller;
 
-import com.example.springcourse.dao.EntityDao;
 import com.example.springcourse.entity.Book;
+import com.example.springcourse.service.EntityService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/books")
 public class BookController {
 
-    private EntityDao entityDao;
+    @Autowired
+    private EntityService entityService;
 
 
-    @GetMapping("/books")
-    public List<Book> findAllBooks() {
-        List<Book> allBooks = (List<Book>) entityDao.findAll();
-        return allBooks;
+
+    @GetMapping("/b")
+    public ResponseEntity<List<Book>> findAllBooks() {
+        return ResponseEntity.ok(entityService.findAllBook());
     }
 
-    @GetMapping("/books/{id}")
-    public Book getBookById(@PathVariable int id) {
-        Book book = entityDao.findById(id).get();
-        return book;
 
-        }
 
 }
