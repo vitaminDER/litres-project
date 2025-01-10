0680a6c6509076404f132d6f20dd0ca6eb4e3c17
Efim Goncharov <efim.goncharov@bk.ru>
2025-01-10 22:42:05 +0500

    )

--- src/main/java/com/example/springcourse/controller/BookController.java
+++ src/main/java/com/example/springcourse/controller/BookController.java
@@ -25,7 +25,6 @@ public class BookController {
     @GetMapping("/b")
     public ResponseEntity<List<Book>> findAllBooks() {
         return ResponseEntity.ok(entityService.findAllBook());
-
     }
 
 
